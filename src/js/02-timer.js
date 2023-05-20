2e3a403089001c50ecd569fa38b5ac92715b6608
StanislavBatov <125702005+StanislavBatov@users.noreply.github.com>
2023-05-20 22:34:00 +0300

    3
    3

---
+++ src/js/02-timer.js
@@ -0,0 +1,61 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const btnStartref = document.querySelector('[data-start]');
+const spanDays = document.querySelector('[data-days]');
+const spanHours = document.querySelector('[data-hours]');
+const spanMinutes = document.querySelector('[data-minutes]');
+const spanSeconds = document.querySelector('[data-seconds]');
+
+btnStartref.addEventListener('click', onBtnStartClick, { once: true} );
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (new Date() > selectedDates[0]) { alert('Please choose a date in the future'); }
+        btnStartref.removeAttribute('disabled');
+        },
+};
+
+const datePicker = flatpickr("#datetime-picker", options);
+
+
+function onBtnStartClick() {
+    const timeDiference = datePicker.selectedDates[0] - Date.now();
+
+    const startInterval = setInterval(() => {
+     const { days, hours, minutes, seconds } = convertMs(datePicker.selectedDates[0] - Date.now());
+        spanDays.textContent = addLeadingZero(days);
+        spanHours.textContent = addLeadingZero(hours);
+        spanMinutes.textContent = addLeadingZero(minutes);
+        spanSeconds.textContent = addLeadingZero(seconds);
+    }, 1000);
+
+    setTimeout(() => { clearInterval(startInterval) }, timeDiference)
+}
+   
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
